38a738fb70d2a3b975f1b97dbbac84bc1fad664b
Chris Groves <chris_groves87@hotmail.co.uk>
2019-05-21 09:20:17 +0000

    question 8

---
+++ directory-ex8.rb
@@ -0,0 +1,60 @@
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  puts "Enter cohort"
+  print_cohort = gets.chomp
+  students.each do |student|
+    if student[:cohort] == print_cohort
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    else
+    end
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+def input_students
+  puts "Please enter the names and cohorts of the students"
+  puts "To finish, just hit return three times"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp.to_sym
+  cohort = gets.chomp.to_sym
+  # while the name is not empty, repeat this code
+  while true do
+    if name.empty? == true && cohort.empty? == true
+      break
+    elsif name.empty? == true && cohort.empty? == false
+      name = "unknown"
+    elsif name.empty? == false && cohort.empty? == true
+      cohort = "unknown"
+    end
+
+    puts "Student name: #{name}"
+    puts "Student cohort: #{cohort}"
+    puts "Are these correct? Enter 'yes' or 'no' then press enter"
+    confirmation = gets.chomp
+      if confirmation == 'yes'
+      # add the student hash to the array
+      students << {name: name, cohort: cohort}
+      puts "Now we have #{students.count} students"
+      else
+      end
+    # get another name from the user
+    name = gets.chomp
+    cohort = gets.chomp
+  end
+  # return the array of students
+  students
+end
+#nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
